4203ba503c6a770d92979af70efab152c26b4e59
SvenK <git+sveto@svenk.org>
2022-01-06 14:44:49 +0100

    Finishing the website upgrade for now.

--- _data/update-data-sources.sh
+++ _data/update-data-sources.sh
@@ -17,7 +17,7 @@ declare -a local_yaml_files=()
 identifier="denktmit-blog"; start $identifier
 prop "title: DenktMit Blog"
 prop "human_url: https://denktmit.de/outreach.html"
-prop "icon: https://denktmit.de/files/sdm/images/logo_blue_without_text.png"
+prop "icon: /assets/icons/code.png"
 data_url="https://denktmit.de/blog-feed.xml"; show data_url
 local_file="denktmit-blog-feed.xml"; show local_file
 download
@@ -26,22 +26,36 @@ download
 local_yaml_files+=("${local_file/.*}.yaml")
 git_add_assets
 
-identifier="papers"; start $identifier
+identifier="scientific_publications"; start $identifier
 prop "title: Publications"
 prop "human_url: https://github.com/svenk/publications/"
-prop "icon: https://upload.wikimedia.org/wikipedia/commons/3/32/OOjs_UI_icon_academic.svg"
+prop "icon: /assets/icons/univ.png"
 data_url="https://raw.githubusercontent.com/svenk/publications/master/Papers/papers-svenk.yaml"
 show data_url
-local_file="$(basename $data_url)"; show local_file
-download
-sed -i "s/papers:/$identifier:/" $local_file # should also assert
+local_file="$identifier.yaml"; show local_file
+# either use some local source:
+cp ../publications/Papers/papers-svenk.yaml $local_file
+# or:
+#download
+#sed -i "s/scientific_publications:/$identifier:/" $local_file # should probably assert
+local_yaml_files+=($local_file)
+git_add_assets
+
+identifier="talks"; start $identifier
+prop "title: Talks"
+prop "human_url: https://github.com/svenk/publications/"
+prop "icon: /assets/icons/talk.png"
+local_file="$identifier.yaml"; show local_file
+# there is no data url and download, crawling is done right here in place.
+../publications/Talks/find-talks.py > $local_file
+# should probably assert: "talks:" is in $local_file.
 local_yaml_files+=($local_file)
 git_add_assets
 
 identifier="t29-blog"; start $identifier
 prop "title: technikum29 Blog"
 prop "human_url: https://technikum29.de/blog/"
-prop "icon: https://technikum29.de/shared/img-v6/touch-icon.png"
+prop "icon: /assets/icons/museum.png"
 data_url="https://technikum29.de/blog/rss.php"; show data_url
 local_file="t29-blog-feed.xml"; show local_file
 download
@@ -52,7 +66,7 @@ git_add_assets
 identifier="uniordner"; start $identifier
 prop "title: Uniordner (historisch)"
 prop "human_url: http://sven.köppel.org/uni/"
-prop "icon: http://sven.köppel.org/favicon.ico"
+prop "icon: /assets/icons/talk.png" # <- because it is mostly talks
 data_url="http://sven.köppel.org/uni/cgi-bin/json-uniordner"; show data_url
 local_file="uniordner.json"; show local_file
 download
