a31ba6c4bfa66eeb2b7f8f19b37f0b10ae150bcd
SvenK <git+sveto@svenk.org>
2024-02-12 03:24:06 +0100

    Starting local blog with anti-AfD post.
    Also included in aggregation system, updated the news.
    Added some minimal design for the blogging.

--- _data/update-data-sources.sh
+++ _data/update-data-sources.sh
@@ -33,7 +33,22 @@ prop "human_url: https://denktmit.de/outreach.html"
 prop "icon: /assets/icons/code.png"
 data_url="https://denktmit.de/blog/index.xml"; show data_url
 local_file="denktmit-blog-feed.xml"; show local_file
-download
+# download # TODO: currently broken, skip download
+prop "skipped_download: True # because target is currently offline!"
+# Since the hugo feed contains no author field and I wrote the majority of blog
+# posts, skip --author-filter=Sven  for the time being
+./rss2yaml.py $local_file --skip-field=description \
+    --out-keyname=$identifier  > ${local_file/.*}.yaml
+local_yaml_files+=("${local_file/.*}.yaml")
+git_add_assets
+
+identifier="svenk-local-blog"; start $identifier
+prop "title: SvenK local Blog"
+prop "human_url: https://svenk.org/posts/"
+prop "icon: /assets/icons/essay.png"
+data_url="http://127.0.0.1:4000/posts-feed.xml"; show data_url
+local_file="svenk-local-blog.xml"; show local_file
+download # need to download since ../posts-feed.xml is only a template
 # Since the hugo feed contains no author field and I wrote the majority of blog
 # posts, skip --author-filter=Sven  for the time being
 ./rss2yaml.py $local_file --skip-field=description \
