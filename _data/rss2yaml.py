85dc6795ec7fc4221fabe659f5541fa8c6f2fa4a
SvenK <git+sveto@svenk.org>
2025-02-01 18:26:23 +0100

    New feed data

--- _data/rss2yaml.py
+++ _data/rss2yaml.py
@@ -6,6 +6,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("xml_filename", type=argparse.FileType('r'))
 parser.add_argument("--author-filter", default="",
     help="Author name to filter on (case insensitive)")
+parser.add_argument("--title-filter", default="",
+    help="Title to filter on (case insensitive), is OR-added to author-filter")
 parser.add_argument("--skip-field", nargs="*",
     default=[], help="Fields to skip in output (ie: description)")
 parser.add_argument("--out-keyname", default=None,
@@ -14,15 +16,22 @@ args = parser.parse_args()
 
 parser = et.XMLParser(resolve_entities=False) # for also slurping not-so-well-formed XMLs
 xml = et.parse(args.xml_filename, parser)
-items = xml.findall("//item")
+items = xml.findall(".//item")
 records = [ { e.tag: e.text for e in item \
                if not e.tag in args.skip_field
             } for item in items ]
+unfiltered_records = records
 
 if args.author_filter:
     records = list(filter(lambda rec: "author" in rec and 
         args.author_filter.lower() in  rec["author"].lower(),
-        records))
+        unfiltered_records))
+
+if args.title_filter:
+    # hoping that we don't duplicate stuff...
+    records += list(filter(lambda rec: "title" in rec and 
+        args.title_filter.lower() in  rec["title"].lower(),
+        unfiltered_records))
     
 out_obj = {args.out_keyname: records} if args.out_keyname else records
 print(yaml.dump(out_obj))
--- _data/update-data-sources.sh
+++ _data/update-data-sources.sh
@@ -21,7 +21,8 @@ prop "human_url: https://anabrid.com/news/"
 prop "icon: /assets/icons/anabrid.png"
 data_url="https://anabrid.com/news/feed.xml"; show data_url
 local_file="anabrid-news-feed.xml"; show local_file
-download
+#download
+prop "skipped_download: True # because target is currently offline!"
 ./rss2yaml.py $local_file --skip-field=description \
     --out-keyname=$identifier  > ${local_file/.*}.yaml
 local_yaml_files+=("${local_file/.*}.yaml")
@@ -34,9 +35,7 @@ prop "icon: /assets/icons/code.png"
 data_url="https://denktmit.de/feed.xml"; show data_url
 local_file="denktmit-blog-feed.xml"; show local_file
 download
-# Since the ~hugo~ 11ty feed contains no author field and I wrote the majority of blog
-# posts, skip --author-filter=Sven  for the time being
-./rss2yaml.py $local_file --skip-field=description \
+./rss2yaml.py $local_file --skip-field=description --author-filter=Sven --title-filter=Sven  \
     --out-keyname=$identifier  > ${local_file/.*}.yaml
 local_yaml_files+=("${local_file/.*}.yaml")
 git_add_assets
@@ -49,8 +48,6 @@ data_url="http://127.0.0.1:4000/posts-feed.xml"; show data_url
 local_file="svenk-local-blog.xml"; show local_file
 #download # need to download since ../posts-feed.xml is only a template
 sed -i 's#http://localhost:4000/#/#g' $local_file # some ugly URL hotfixing
-# Since the hugo feed contains no author field and I wrote the majority of blog
-# posts, skip --author-filter=Sven  for the time being
 ./rss2yaml.py $local_file --skip-field=description \
     --out-keyname=$identifier  > ${local_file/.*}.yaml
 local_yaml_files+=("${local_file/.*}.yaml")
